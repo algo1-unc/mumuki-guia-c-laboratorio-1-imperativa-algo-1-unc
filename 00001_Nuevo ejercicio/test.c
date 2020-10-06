d08289e683d415c3f01f89f8b8f368eac4a8a26f
hernan.maina@mi.unc.edu.ar <hernan.maina@mi.unc.edu.ar>
2020-10-06 00:41:54 +0000

    Mumuki Export on 2020-10-06 00:41:54 +0000

--- 00001_Nuevo ejercicio/test.c
+++ 00001_Nuevo ejercicio/test.c
@@ -1,5 +1,5 @@
 describe ("Descripción del grupo") {
   it ("Estado 1 x->1, y->1, z->1") {
-    should_int(estado1()) be equal to(2);
+    should_int(estado1(int x, int y, int z)) be equal to(2);
   } end
 } end
