8ed27bb0cc237b3a9aacc64f7d3053dd368023f2
hernan.maina@mi.unc.edu.ar <hernan.maina@mi.unc.edu.ar>
2020-10-06 20:21:41 +0000

    Mumuki Export on 2020-10-06 20:21:41 +0000

--- 00001_Entrada-Salida/extra.c
+++ 00001_Entrada-Salida/extra.c
@@ -1,21 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+
 int my_exp1(int x, int y) {
     int res = x + y + 1;
-    printf("%d", res)
+    printf("%d", res);
     return res;
 }
 
 int my_exp2(int x, int y, int z) {
-    return z * z + y * 45 - 15 * x;
+    int res = z * z + y * 45 - 15 * x;
+    printf("%d", res);
+    return res;
 }
 
 bool my_exp3(int x, int y) {
-    return  (y - 2)  == (x * 3 + 1) % 5;
+    bool res = (y - 2)  == (x * 3 + 1) % 5;
+    printf("%d", res);
+    return  res;
 }
 
 int my_exp4(int x, int y) {
-    return y / 2 * x;
+    int res = y / 2 * x;
+    printf("%d", res);
+    return res;
 }
 
 bool my_exp5(int x, int y, int z) {
-    return y < x * z;
+    bool res = y < x * z;
+    printf("%d", res);
+    return res;
 }
