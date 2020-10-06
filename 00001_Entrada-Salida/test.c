135e8490d4a00661f76f2daf6ba3bb4ab9a20a50
hernan.maina@mi.unc.edu.ar <hernan.maina@mi.unc.edu.ar>
2020-10-06 20:16:02 +0000

    Mumuki Export on 2020-10-06 20:16:02 +0000

--- 00001_Entrada-Salida/test.c
+++ 00001_Entrada-Salida/test.c
@@ -1,31 +1,34 @@
 /*...extra...*/
 
 describe ("Test de estado (x,y,z)") {
-  before {
-    puts("Init X");
-  } end
   
-  after {
-    puts("after 3");
-  } end
-
+  int out = my_exp1(X_VAR,Y_VAR);
+  printf("Resultado final %d", out);
   it ("exp1: (x + y + 1)") {
-    should_int(exp1()) be equal to(my_exp1(X_VAR,Y_VAR));
+    should_int(exp1()) be equal to(out);
   } end
   
+  int out = my_exp2(X_VAR,Y_VAR,Z_VAR);
+  printf("Resultado final %d", out);
   it ("exp2: (z * z + y * 45 - 15 * x)") {
-    should_int(exp2()) be equal to(my_exp2(X_VAR,Y_VAR,Z_VAR));
+    should_int(exp2()) be equal to(out);
   } end
   
+  bool out = my_exp3(X_VAR,Y_VAR);
+  printf("Resultado final %d", out);
   it ("exp3: (y - 2 = (x * 3 + 1) % 5)") {
-    should_bool(exp3()) be equal to(my_exp3(X_VAR,Y_VAR));
+    should_bool(exp3()) be equal to(out);
   } end
   
+  int out = my_exp4(X_VAR,Y_VAR);
+  printf("Resultado final %d", out);
   it ("exp4: (y / 2 * x)") {
-    should_int(exp4()) be equal to(my_exp4(X_VAR,Y_VAR));
+    should_int(exp4()) be equal to(out);
   } end
   
+  bool out = my_exp5(X_VAR,Y_VAR,Z_VAR);
+  printf("Resultado final %d", out);
   it ("exp5: (y < x * z)") {
-    should_bool(exp5()) be equal to(my_exp5(X_VAR,Y_VAR,Z_VAR));
+    should_bool(exp5()) be equal to(out);
   } end
 } end
--- 00002_Debugging/test.c
+++ 00002_Debugging/test.c
@@ -1,14 +1,14 @@
 describe ("Test para estado (X,Y,X,B,W)") {
   
-  it ("exp1: (x % 4 == 0)") {
+  it ("exp1 debe ser True") {
     should_bool(exp1()) be equal to(true);
   } end
   
-  it ("exp2: (x + y == 0 && y - x == (-1) * z)") {
+  it ("exp2 debe ser True") {
     should_bool(exp2()) be equal to(true);
   } end
   
-  it ("exp3: (not b && w)") {
+  it ("exp3 debe ser False") {
     should_bool(exp3()) be equal to(false);
   } end
   
