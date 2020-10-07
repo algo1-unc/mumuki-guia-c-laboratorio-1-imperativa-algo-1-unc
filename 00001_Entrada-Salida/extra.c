a9449ed2452159255c1db0e37f7b0a1fae5d6fe9
hernan.maina@mi.unc.edu.ar <hernan.maina@mi.unc.edu.ar>
2020-10-07 13:42:41 +0000

    Mumuki Export on 2020-10-07 13:42:41 +0000

--- 00001_Entrada-Salida/extra.c
+++ 00001_Entrada-Salida/extra.c
@@ -14,50 +14,36 @@ char* genState(const char* name_of_test, const int x, const int y, const int z) 
     return message;
 }
 
-char* genIntDebugMsg(const char* function_name , const int val_returned) {
-    /*
-        Toma el nombre de la funcion y su resultado (Int), 
-        y crea el mensaje de devolucion.
-    */
-    
-    char* message;
-    asprintf(&message, "%s(), devolvio '%d'", function_name, val_returned);
-    return message;
-}
-
-char* genBoolDebugMsg(const char* function_name , const bool val_returned) {
-    /*
-        Toma el nombre de la funcion y su resultado (Bool), 
-        y crea el mensaje de devolucion.
-    */
-    
-    char* message;
-    asprintf(&message, "%s(), devolvio '%s'", 
-        function_name, val_returned ? "true" : "false");
-    return message;
-}
-
-
-char* genIntMsg(const char* function_name , const int val_expected, const int val_returned) {
+char* genIntMsg(const char* function_name , const int val_expected, const int val_returned, bool debug_mode) {
     /*
         Toma el nombre de la funcion y el valor de resultado esperado (Int), 
         y crea el mensaje de devolucion para el test unitario.
     */
     
     char* message;
-    asprintf(&message, "Dado %s(). Valor esperado:%d, Valor retornado:%d.", 
-        function_name, val_expected, val_returned);
+    if (debug_mode) {
+      asprintf(&message, "%s(), devolvio '%d'", function_name, val_returned);
+    } else {
+      asprintf(&message, "Dado %s(). Valor esperado:%d, Valor retornado:%d.", 
+        function_name, val_expected, val_returned);  
+    }
+    
     return message;
 }
 
-char* genBoolMsg(const char* function_name , const bool val_expected, const bool val_returned) {
+char* genBoolMsg(const char* function_name , const bool val_expected, const bool val_returned, bool debug_mode) {
     /*
         Toma el nombre de la funcion y el valor de resultado esperado (Bool), 
         y crea el mensaje de devolucion para el test unitario.
     */
     char* message;
-    asprintf(&message, "Dado %s(). Valor esperado:%s, Valor retornado:%s.", 
-        function_name, val_expected ? "true" : "false", val_returned ? "true" : "false");
+    if (debug_mode) {
+      asprintf(&message, "%s(), devolvio '%s'", function_name, val_returned ? "true" : "false");
+    } else {
+      asprintf(&message, "Dado %s(). Valor esperado:%s, Valor retornado:%s.", 
+        function_name, val_expected ? "true" : "false", val_returned ? "true" : "false");  
+    }
+    
     return message;
 }
 
--- 00001_Entrada-Salida/test.c
+++ 00001_Entrada-Salida/test.c
@@ -4,27 +4,27 @@ if (!DEBUG) {
   
   describe (genState("Test", X_VAR, Y_VAR, Z_VAR)) {
     int expected1 = my_exp1(X_VAR,Y_VAR);
-    it (genIntMsg("exp1", expected1, exp1())) {
+    it (genIntMsg("exp1", expected1, exp1(), DEBUG)) {
       should_int(exp1()) be equal to(expected1);
     } end
     
     int expected2 = my_exp2(X_VAR,Y_VAR,Z_VAR);
-    it (genIntMsg("exp2", expected2, exp2())) {
+    it (genIntMsg("exp2", expected2, exp2(), DEBUG)) {
       should_int(exp2()) be equal to(expected2);
     } end
     
     bool expected3 = my_exp3(X_VAR,Y_VAR);
-    it (genBoolMsg("exp3", expected3, exp3())) {
+    it (genBoolMsg("exp3", expected3, exp3(), DEBUG)) {
       should_bool(exp3()) be equal to(expected3);
     } end
     
     int expected4 = my_exp4(X_VAR,Y_VAR);
-    it (genIntMsg("exp4", expected4, exp4())) {
+    it (genIntMsg("exp4", expected4, exp4(), DEBUG)) {
       should_int(exp4()) be equal to(expected4);
     } end
     
     bool expected5 = my_exp5(X_VAR,Y_VAR,Z_VAR);
-    it (genBoolMsg("exp5", expected5, exp5())) {
+    it (genBoolMsg("exp5", expected5, exp5(), DEBUG)) {
       should_bool(exp5()) be equal to(expected5);
     } end
   } end
@@ -32,23 +32,26 @@ if (!DEBUG) {
 } else {
   
   describe (genState("Modo DEBUG", X_VAR, Y_VAR, Z_VAR)) {
-    it (genIntDebugMsg("exp1", exp1())) {
+    int dbgint = 0;
+    bool dbgbool = false;
+    
+    it (genIntMsg("exp1", dbgint, exp1(), DEBUG)) {
       should_bool(false) be equal to(true);
     } end
     
-    it (genIntDebugMsg("exp2",exp2())) {
+    it (genIntMsg("exp2", dbgint, exp2(), DEBUG)) {
       should_bool(false) be equal to(true);
     } end
     
-    it (genBoolDebugMsg("exp3", exp3())) {
+    it (genBoolMsg("exp3", dbgbool, exp3(), DEBUG)) {
       should_bool(false) be equal to(true);
     } end
     
-    it (genIntDebugMsg("exp4", exp4())) {
+    it (genIntMsg("exp4", dbgint, exp4(), DEBUG)) {
       should_bool(false) be equal to(true);
     } end
     
-    it (genBoolDebugMsg("exp5", exp5())) {
+    it (genBoolMsg("exp5", dbgbool, exp5(), DEBUG)) {
       should_bool(false) be equal to(true);
     } end
   } end
