0ae68d285674d8eeb099d1c856682aaac5b13d54
hernan.maina@mi.unc.edu.ar <hernan.maina@mi.unc.edu.ar>
2020-10-09 14:12:27 +0000

    Mumuki Export on 2020-10-09 14:12:27 +0000

--- 00001_Entrada-Salida/description.md
+++ 00001_Entrada-Salida/description.md
@@ -1,9 +1,17 @@
-Hace un programa en C, que solicite el ingreso de los valores de las variables x, y, z e imprima el resultado de las siguientes expresiones. Completá los de las expresiones a continuació para los dos estados dados
-estado1 = x->7, y->3, z->5
-estado2 = x->1, y->10, z->8
+Dada las siguientes expresiones:
 
-x + y + 1
-z * z + y * 45 - 15 * x
-y - 2 = (x * 3 + 1) % 5
-y / 2 * x
-y < x * z
+>* exp1 = ```(x + y + 1)```
+* exp2 = ```z * z + y * 45 - 15 * x```
+* exp3 = ```y - 2 == (x * 3 + 1) % 5```
+* exp4 = ```y / 2 * x```
+* exp5 = ```y < x * z```
+
+Codifique las respectivas funciones en C, para que puedan ser evaluadas en los estados:
+
+* Estado1: ```(x->7, y->3, z->5)```
+* Estado2: ```(x->1, y->10, z->8)```
+
+
+**IMPORTANTE:** Para probar los resultado de las funciones codificadas, active el modo consulta asignando `DEBUG true`. Una vez concluida su consulta es importante que vuelva a asignar `DEBUG false` para poder completar la tarea correctamente.
+
+> **NOTA:** No modifique el nombre de las funciones de la plantilla presentada.
---
+++ 00001_Entrada-Salida/extra.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+// Funciones para generar mensaje de test
+
+char* genState(const char* name_of_test, const int x, const int y, const int z) {
+    /*
+        Formatea string con valores de estados pasados
+    */
+    
+    char* message;
+    asprintf(&message, "%s para estado (x->%d, y->%d, z->%d)", name_of_test, x,y,z);
+    return message;
+}
+
+char* genIntMsg(const char* function_name , const int val_expected, const int val_returned, bool debug_mode) {
+    /*
+        Toma el nombre de la funcion y el valor de resultado esperado (Int), 
+        y crea el mensaje de devolucion para el test unitario.
+    */
+    
+    char* message;
+    if (debug_mode) {
+      asprintf(&message, "%s(), devolvio '%d'", function_name, val_returned);
+    } else {
+      asprintf(&message, "Dado %s(). Valor esperado:%d, Valor retornado:%d.", 
+        function_name, val_expected, val_returned);  
+    }
+    
+    return message;
+}
+
+char* genBoolMsg(const char* function_name , const bool val_expected, const bool val_returned, bool debug_mode) {
+    /*
+        Toma el nombre de la funcion y el valor de resultado esperado (Bool), 
+        y crea el mensaje de devolucion para el test unitario.
+    */
+    char* message;
+    if (debug_mode) {
+      asprintf(&message, "%s(), devolvio '%s'", function_name, val_returned ? "true" : "false");
+    } else {
+      asprintf(&message, "Dado %s(). Valor esperado:%s, Valor retornado:%s.", 
+        function_name, val_expected ? "true" : "false", val_returned ? "true" : "false");  
+    }
+    
+    return message;
+}
+
+
+// Funciones para testeo de ejercicio
+
+int my_exp1(int x, int y) {
+    int res = x + y + 1;
+    return res;
+}
+
+int my_exp2(int x, int y, int z) {
+    int res = z * z + y * 45 - 15 * x;
+    return res;
+}
+
+bool my_exp3(int x, int y) {
+    bool res = (y - 2)  == (x * 3 + 1) % 5;
+    printf("%d", res);
+    return  res;
+}
+
+int my_exp4(int x, int y) {
+    int res = y / 2 * x;
+    return res;
+}
+
+bool my_exp5(int x, int y, int z) {
+    bool res = y < x * z;
+    return res;
+}
+
