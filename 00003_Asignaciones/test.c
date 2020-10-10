168039b3fde5b4f18deb15288cccba1e771d64fe
hernan.maina@mi.unc.edu.ar <hernan.maina@mi.unc.edu.ar>
2020-10-10 22:11:17 +0000

    Mumuki Export on 2020-10-10 22:11:17 +0000

--- 00003_Asignaciones/test.c
+++ 00003_Asignaciones/test.c
@@ -32,7 +32,7 @@ if (CONSULTA) {
     describe (genState("Función 'exp_1c'", X_VAR, Y_VAR, CONSULTA)) {
         x_student = X_VAR;
         y_student = Y_VAR;
-        exp_1c(&x_student);
+        exp_1c(&x_student, &y_student);
             
         it (genMsg("", 0, 0, x_student, y_student, CONSULTA)) {
             should_bool(false) be equal to(true);
@@ -47,6 +47,22 @@ if (CONSULTA) {
     x_student = 2;
     y_student = 5;
     x_test = 2;
+    y_test = 5; 
+
+    describe (genState("Función 'exp_1a'", x_test, y_test, CONSULTA)) {
+        
+        exp_1a(&x_student);
+        my_exp_1a(&x_test);
+            
+        it (genMsg("x", x_test, y_test, x_student, y_student, CONSULTA)) {
+            should_int(x_student) be equal to(x_test);
+        } end
+
+    } end
+    
+    x_student = 1;
+    y_student = 5;
+    x_test = 1;
     y_test = 5; 
 
     describe (genState("Función 'exp_1a'", x_test, y_test, CONSULTA)) {
@@ -81,6 +97,26 @@ if (CONSULTA) {
         } end
 
     } end
+    
+    x_student = 8;
+    y_student = 3;
+    x_test = 8;
+    y_test = 3; 
+
+    describe (genState("Función 'exp_1b'", x_test, y_test, CONSULTA)) {
+
+        exp_1b(&x_student, &y_student);
+        my_exp_1b(&x_test, &y_test);
+            
+        it (genMsg("x", x_test, y_test, x_student, y_student, CONSULTA)) {
+            should_int(x_student) be equal to(x_test);
+        } end
+
+        it (genMsg("y", x_test, y_test, x_student, y_student, CONSULTA)) {
+            should_int(y_student) be equal to(y_test);
+        } end
+
+    } end
 
 
     // Testeo exp_1c
@@ -89,6 +125,26 @@ if (CONSULTA) {
     y_student = 5;
     x_test = 2;
     y_test = 5; 
+
+    describe (genState("Función 'exp_1c'", x_test, y_test, CONSULTA)) {
+
+        exp_1c(&x_student, &y_student);
+        my_exp_1c(&x_test, &y_test);
+            
+        it (genMsg("x", x_test, y_test, x_student, y_student, CONSULTA)) {
+            should_int(x_student) be equal to(x_test);
+        } end
+
+        it (genMsg("y", x_test, y_test, x_student, y_student, CONSULTA)) {
+            should_int(y_student) be equal to(y_test);
+        } end
+
+    } end
+    
+    x_student = 8;
+    y_student = 3;
+    x_test = 8;
+    y_test = 3; 
 
     describe (genState("Función 'exp_1c'", x_test, y_test, CONSULTA)) {
 
