267388c6fd940a30c4b1397a5b40a76d8e3dbbe7
hernan.maina@mi.unc.edu.ar <hernan.maina@mi.unc.edu.ar>
2020-10-12 01:49:19 +0000

    Mumuki Export on 2020-10-12 01:49:19 +0000

--- 00009_Hay escalera_/description.md
+++ 00009_Hay escalera_/description.md
@@ -1,18 +1,21 @@
-Queremos saber si dadas dos cartas de un mazo español (con 8's y 9's), hay un escalera.
+Queremos saber si dadas tres cartas de un mazo español (con 8's y 9's), hay un escalera.
 
  
 > ```javascript
-> hayEscalera(1,5)
+> hayEscalera(1,5,10)
 > false
 >
-> hayEscalera(9,10)
+> hayEscalera(9,10,8)
 > true
 >
-> hayEscalera(5,4)
+> hayEscalera(5,4,3)
 > true
 >
-> hayEscalera(1,1)
+> hayEscalera(1,1,10)
+> false
+>
+> hayEscalera(12,1,11)
 > false
 > ```
 
-Programá la función `hayEscalera` que toma como argumentos dos enteros y retorne un booleano.
+Programá la función `hayEscalera` que toma como argumentos tres enteros y retorne un booleano.
--- 00009_Hay escalera_/test.c
+++ 00009_Hay escalera_/test.c
@@ -1,19 +1,35 @@
 describe ("Test negativos") {
   
-  it ("Dado hayEscalera(1,1), debe devolver 'false'") {
-    should_bool(hayEscalera(1,1)) be equal to(false);
+  it ("Dado hayEscalera(1,1,10), debe devolver 'false'") {
+    should_bool(hayEscalera(1,1,10)) be equal to(false);
   } end
 
-  it ("Dado hayEscalera(10,5), debe devolver 'false'") {
-    should_bool(hayEscalera(10,5)) be equal to(false);
+  it ("Dado hayEscalera(5,3,3), debe devolver 'false'") {
+    should_bool(hayEscalera(5,3,3)) be equal to(false);
   } end
 
-  it ("Dado hayEscalera(1,12), debe devolver 'false'") {
-    should_bool(hayEscalera(1,12)) be equal to(false);
+  it ("Dado hayEscalera(5,4,1), debe devolver 'false'") {
+    should_bool(hayEscalera(5,4,1)) be equal to(false);
   } end
 
-  it ("Dado hayEscalera(7,9), debe devolver 'false'") {
-    should_bool(hayEscalera(1,12)) be equal to(false);
+  it ("Dado hayEscalera(5,1,4), debe devolver 'false'") {
+    should_bool(hayEscalera(5,1,4)) be equal to(false);
+  } end
+
+  it ("Dado hayEscalera(1,4,2), debe devolver 'false'") {
+    should_bool(hayEscalera(1,4,2)) be equal to(false);
+  } end
+
+  it ("Dado hayEscalera(7,7,7), debe devolver 'false'") {
+    should_bool(hayEscalera(7,7,7)) be equal to(false);
+  } end
+
+  it ("Dado hayEscalera(7,3,7), debe devolver 'false'") {
+    should_bool(hayEscalera(7,3,7)) be equal to(false);
+  } end
+
+  it ("Dado hayEscalera(12,3,7), debe devolver 'false'") {
+    should_bool(hayEscalera(12,3,7)) be equal to(false);
   } end
 
 } end
@@ -21,20 +37,24 @@ describe ("Test negativos") {
 
 describe ("Test positivos") {
   
-  it ("Dado hayEscalera(3,4), debe devolver 'true'") {
-    should_bool(hayEscalera(3,4)) be equal to(true);
+  it ("Dado hayEscalera(3,4,5), debe devolver 'true'") {
+    should_bool(hayEscalera(3,4,5)) be equal to(true);
   } end
 
-  it ("Dado hayEscalera(10,9), debe devolver 'true'") {
-    should_bool(hayEscalera(10,9)) be equal to(true);
+  it ("Dado hayEscalera(10,9,8), debe devolver 'true'") {
+    should_bool(hayEscalera(10,9,8)) be equal to(true);
   } end
 
-  it ("Dado hayEscalera(11,12), debe devolver 'true'") {
-    should_bool(hayEscalera(11,12)) be equal to(true);
+  it ("Dado hayEscalera(11,12,10), debe devolver 'true'") {
+    should_bool(hayEscalera(11,12,10)) be equal to(true);
   } end
 
-  it ("Dado hayEscalera(6,7), debe devolver 'true'") {
-    should_bool(hayEscalera(6,7)) be equal to(true);
+  it ("Dado hayEscalera(8,6,7), debe devolver 'true'") {
+    should_bool(hayEscalera(8,6,7)) be equal to(true);
+  } end
+
+  it ("Dado hayEscalera(8,9,7), debe devolver 'true'") {
+    should_bool(hayEscalera(8,9,7)) be equal to(true);
   } end
 
 } end
